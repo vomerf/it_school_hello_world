6665487c2cf5434a487dde71ce0e5f7749db9300
Nikita <vomerff@gmail.com>
2025-09-01 08:42:59 +0700

    update cd

--- app/api/endpoints/__init__.py
+++ app/api/endpoints/__init__.py
@@ -1,1 +1,2 @@
 from .hero import router as hero_router
+from .health_check import router as health_router
--- app/api/routers.py
+++ app/api/routers.py
@@ -1,7 +1,8 @@
 from fastapi import APIRouter
 
 from .endpoints import hero_router
-
+from .endpoints import health_router
 main_router = APIRouter()
 
 main_router.include_router(hero_router, prefix='/hero', tags=['hero'])
+main_router.include_router(health_router, tags=['health_check'])
